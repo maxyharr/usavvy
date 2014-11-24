1e9f06cb2da1fa63fb4b17ad28460df45e31a1b6
Max Harris <mjharris@colorado.edu>
2014-11-24 09:17:39 -0500

    uploading a picture works (front-end only)

--- USavvy/HostFormViewController.swift
+++ USavvy/HostFormViewController.swift
@@ -8,16 +8,97 @@
 
 import UIKit
 
-class HostFormViewController: UIViewController {
-
+class HostFormViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
     @IBOutlet weak var titleField: UITextField!
     
     @IBOutlet weak var guestsField: UITextField!
     @IBOutlet weak var hoursField: UITextField!
     @IBOutlet weak var experienceImageView: UIImageView!
     
+    @IBOutlet weak var uploadPictureButton: UIButton!
+    var picker:UIImagePickerController? = UIImagePickerController()
+    var popover:UIPopoverController? = nil
+    
     @IBAction func uploadBackgroundPicture(sender: AnyObject) {
+        picker?.delegate = self
+        
+        var alert:UIAlertController=UIAlertController(title: "Choose Image", message: nil, preferredStyle: UIAlertControllerStyle.ActionSheet)
+        
+        var cameraAction = UIAlertAction(title: "Camera", style: UIAlertActionStyle.Default)
+            {
+                UIAlertAction in
+                self.openCamera()
+                
+        }
+        var gallaryAction = UIAlertAction(title: "Gallary", style: UIAlertActionStyle.Default)
+            {
+                UIAlertAction in
+                self.openGallary()
+        }
+        var cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel)
+            {
+                UIAlertAction in
+                
+        }
+        // Add the actions
+        alert.addAction(cameraAction)
+        alert.addAction(gallaryAction)
+        alert.addAction(cancelAction)
+        // Present the actionsheet
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone
+        {
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+        else
+        {
+            popover=UIPopoverController(contentViewController: alert)
+            popover!.presentPopoverFromRect(uploadPictureButton.frame, inView: self.view, permittedArrowDirections: UIPopoverArrowDirection.Any, animated: true)
+        }
+        
     }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+    
+        println("finished picking image")
+        picker .dismissViewControllerAnimated(true, completion: nil)
+        
+        self.experienceImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        self.experienceImageView.contentMode = UIViewContentMode.ScaleAspectFit
+        //sets the selected image to image view
+        
+    }
+    
+
+    func imagePickerControllerDidCancel(picker: UIImagePickerController!) {
+        println("picker cancel.")
+    }
+
+    
+    func openCamera() {
+        println("opened camera")
+            if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)) {
+                picker!.sourceType = UIImagePickerControllerSourceType.Camera
+                self .presentViewController(picker!, animated: true, completion: nil)
+            }
+    }
+    
+    func openGallary()
+    {
+        println("opened gallery")
+        
+        picker!.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone
+        {
+            self.presentViewController(picker!, animated: true, completion: nil)
+        }
+        else
+        {
+            popover=UIPopoverController(contentViewController: picker!)
+            popover!.presentPopoverFromRect(uploadPictureButton.frame, inView: self.view, permittedArrowDirections: UIPopoverArrowDirection.Any, animated: true)
+        }
+    }
+    
     @IBAction func completePosting(sender: AnyObject) {
         // return to previous screen
         
